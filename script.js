5dccee496ea359c6e9c018a174380ddea465aa73
marc <marcrsfeir@gmail.com>
2026-06-04 18:54:23 +0400

    Smooth scrolling

--- script.js
+++ script.js
@@ -22,3 +22,13 @@ if (backdropVideo) {
     }
   });
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
